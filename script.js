5df71eb473b9e5396e344b55a0075df85200ef24
Rajas Paranjpe <paranjperajas@gmail.com>
2020-11-21 13:39:57 -0600

    Progress

--- script.js
+++ script.js
@@ -5,6 +5,13 @@ const form = document.getElementById('form')
 const existSpan = document.getElementById('form__exist-span')
 const srcSpan = document.getElementById('form__src-span')
 const destSpan = document.getElementById('form__dest-span')
+const progressDiv = document.getElementById('progress')
+const gridCopied = document.getElementById('grid__copied')
+const gridDuplicate = document.getElementById('grid__copied')
+const gridRemaining = document.getElementById('grid__copied')
+const textareaCopied = document.getElementById('grid__copied-list')
+const textareaDuplicate = document.getElementById('grid__copied-duplicate')
+const textareaRemaining = document.getElementById('grid__copied-remaining')
 
 form.addEventListener('submit', e => {
     e.preventDefault()
@@ -66,4 +73,39 @@ Object.values(dirs).forEach(dir => {
         }
     })
     dir.input.addEventListener('input', checkDirectory)
+})
+
+// TODO: remove this
+form.exist.value = 'C:/users/rajas/source/repos/smart-photo-copier/test/dest'
+form.src.value = 'C:/users/rajas/source/repos/smart-photo-copier/test/src/b'
+form.dest.value = 'C:/users/rajas/source/repos/smart-photo-copier/test/dest/b'
+form.copy.disabled = false
+
+const filesCopied = []
+const duplicateFiles = []
+const remainingFiles = []
+const updateProgress = () => {
+    gridCopied.innerText = filesCopied.length
+    duplicateFiles.innerText = filesCopied.length
+    remainingFiles.innerText = filesCopied.length
+    textareaCopied.innerText = filesCopied.join('\n')
+    textareaDuplicate.innerText = duplicateFiles.join('\n')
+    textareaRemaining.innerText = remainingFiles.join('\n')
+}
+
+form.copy.addEventListener('click', () => {
+    // Disable copy button
+    form.copy.disabled = true
+    // Disable everything
+    for (const { button, input } of Object.values(dirs)) {
+        button.disabled = true
+        input.disabled = true
+    }
+    // Show progress
+    progressDiv.classList.remove('hidden')
+    // Reset progress
+    filesCopied.length = 0
+    duplicateFiles.length = 0
+    remainingFiles.length = 0
+    updateProgress()
 })
